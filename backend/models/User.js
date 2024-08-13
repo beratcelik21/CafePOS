72b5033181d4eea59760ab8369de3deb6a4d7915
Berat Celik <beratcelik198007@gmail.com>
2024-08-13 10:57:00 +0200

    frontend

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: DataTypes.ENUM('waiter', 'cashier', 'kitchen', 'admin'),
+        allowNull: false,
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = User;
